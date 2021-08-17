ced551a82832ec08b7f6236109443dd7adeb3851
Deepakkumar Murugan <73244530+Deepakkumar14@users.noreply.github.com>
2021-08-17 19:27:26 +0530

    edited

--- bank.sql
+++ bank.sql
@@ -1,16 +1,21 @@
-show databases;
-use Zohobank1;
-create database Zohobank1;
+drop database Zohobank;
+use Zohobank;
+create database Zohobank;
+select * from transaction_details;
 select * from customer_details;
 use bank4;
+drop table transaction_details;
+update customer_details set status='Deactive' where customer_id=6;update account_details set status='Deactive' where customer_id=6;
+UPDATE account_details ad JOIN customer_details cd ON ad.customer_id = cd.customer_id  SET ad.status="Deactive"&&cd.status="Deactive" WHERE cd.customer_id=12;
 SELECT IDENT_CURRENT(customer_details);
 CREATE TABLE customer_details
        (
-       customer_id int not null auto_increment,
+       customer_id int not null auto_increment ,
        full_name VARCHAR(30)not null,
        city VARCHAR(15)not null,
+       status varchar(10) not null check (status in ('Active', 'Deactive')),
        primary key(customer_id));
-       
+       alter table customer_details alter status  set default 'Active'; 
       
  CREATE TABLE account_details
    (  
@@ -22,21 +27,23 @@ CREATE TABLE customer_details
       CONSTRAINT ac_number_pk PRIMARY KEY(account_number),
     CONSTRAINT account_id_fk FOREIGN KEY(customer_id) REFERENCES customer_details(customer_id) on update cascade); 
       alter table account_details auto_increment = 1000;
-      alter table account_details alter status  set default 'Activate';
+     alter table account_details alter status  set default 'Active';
       
       CREATE TABLE transaction_details
     (   
-     transaction_number VARCHAR(6) ,
-     account_number VARCHAR(6),
-     dot DATE,
-     transaction_type VARCHAR(20),
-     transaction_amount bigint(7),    
-     CONSTRAINT tnumber_pk PRIMARY KEY(tranasction_number),
-     CONSTRAINT acnumber_fk FOREIGN KEY(account_number) REFERENCES account_details(account_number)  
-    );
+     transaction_number bigint auto_increment not null ,
+     customer_id int not null ,
+     account_number bigint not null ,
+      transaction_type VARCHAR(20) not null,
+     transaction_amount bigint not null, 
+     date bigint,
+     status varchar(20) not null,
+     CONSTRAINT tnumber_pk PRIMARY KEY(transaction_number),
+     CONSTRAINT acnumber_fk FOREIGN KEY(customer_id,account_number) REFERENCES account_details(customer_id,account_number) );
+    alter table transaction_details auto_increment = 1000;
       
        INSERT INTO customer_details(full_name,city) VALUES('Dhatchinamoorthi','karaikudi');
-       INSERT INTO customer_details(full_name,city) VALUES('Janet','karur');
+       INSERT INTO customer_details(full_name,city) VALUES('Janet1','karur');
        INSERT INTO customer_details(full_name,city) VALUES
 	   ('Arun','paramakudi'),
 	  ('vijay','sivagangai'),
